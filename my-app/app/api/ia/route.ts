0647dc9769ffda6d4c1de8d4fd19c289bac54766
benjamin martinez <benjamartinez454gmail.com>
2025-12-09 20:17:13 -0300

    voy a agregar gpt4free

--- my-app/app/api/ia/route.ts
+++ my-app/app/api/ia/route.ts
@@ -1,6 +1,20 @@
 import { NextResponse } from "next/server";
 import OpenAI from "openai";
 
+
+export async function POST(request: Request) {
+const { G4F } = import ("g4f");
+const g4f = new G4F();
+const messages = [
+    { role: "gasista matriculado", content: "eres un gasista experto"}
+];
+g4f.chatCompletion(messages).then(console.log);
+}
+
+// https://github.com/VictorMRojas/g4f-ts?tab=readme-ov-file#installation
+// https://github.com/xtekky/gpt4free?tab=readme-ov-file#installation
+
+
 // agrega un post
 export async function POST(request: Request) {
   const client = new OpenAI({
