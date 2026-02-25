83842128642b1a820ddd8119fdcd11f36711117c
benjaminvasquez07 <benjaminvasquez289@email.com>
2026-02-25 13:41:04 -0300

    hice el test de cotizacion con IA

--- my-app/tests/ia.spec.ts
+++ my-app/tests/ia.spec.ts
@@ -1,9 +1,25 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
 test.describe("Cotizacion automatica - Implementacion IA Chat", () => {
-  test("Cotizacion automatica - Implementacion IA Chat- 6 puntos", async ({ page }) => {
-
-
-
+  test("Cotizacion automatica - Implementacion IA Chat- 6 puntos", async ({
+    page,
+  }) => {
+    await page.goto("http://localhost:4000/");
+    await page
+      .getByRole("button", { name: "Solicitar Cotización Instantá" })
+      .click();
+    await expect(page.getByRole("main")).toContainText(
+      "¡Hola! Dejá tu consulta y nuestro gasista matriculado te responderá a la brevedad para coordinar tu servicio.",
+    );
+    await page
+      .getByRole("textbox", { name: "Escribe tu consulta aquí" })
+      .click();
+    await page
+      .getByRole("textbox", { name: "Escribe tu consulta aquí" })
+      .fill("hola colsulta de gas");
+    await page.getByRole("button", { name: "Enviar" }).click();
+    await expect(page.getByRole("main")).toContainText(
+      "¡Hola! Soy gasista matriculado.",
+    );
   });
 });
