096acbdb80bc739f1ddc15fbced852fdc62bd1c3
benjaminvasquez07 <benjaminvasquez289@email.com>
2026-02-25 17:44:59 -0300

    agregue el URL del csv en un archivo .env.local

--- my-app/app/api/resenas/route.ts
+++ my-app/app/api/resenas/route.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  const url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTHN2NSBC6aQvHuWIszTx9qlW8oluc6VAom4sl0U7J3aMLsMsZMz91uS86ImbAcpdnNglS17yN4IaBd/pub?output=csv";
+  const url = process.env.GOOGLE_SHEET_CSV_URL!;
 
   const response = await fetch(url);
   const text = await response.text();
